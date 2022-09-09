b2cd5c852afbd6a628bb89041a92a415c2f69236
C0433735 <C0433735@online.camosun.ca>
2022-09-09 15:11:05 -0700

    Updated search code

--- search_string.py
+++ search_string.py
@@ -1,6 +1,11 @@
 search_string = input('Enter your search string:')
 search_interval = input('Enter the interval:')
 search_interval = int(search_interval)
-if type(search_interval) is int:
-  if search_interval > 0:
-    print(search_string[::search_interval])
+
+if search_interval > 0 and search_interval < len(search_string):
+  count = 1
+  for char in search_string:
+    if count % search_interval == 0:
+      print(char)
+    count += 1
+
