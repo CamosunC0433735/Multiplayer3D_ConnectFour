b9a40df5cd271659ef0126c5dab728bf4b22551a
C0433735 <C0433735@online.camosun.ca>
2022-09-09 15:27:07 -0700

    help lol idk why my files arent there

---
+++ search_string_negative.py
@@ -0,0 +1,12 @@
+search_string = input('Enter your search string:')
+search_interval = input('Enter the interval:')
+search_interval = int(search_interval)
+
+if search_interval > 0 and search_interval < len(search_string):
+  count = 1
+  for char in search_string:
+    if count % search_interval != 0:
+      print(char)
+    count += 1
+else:
+  print('invalid interval')
